ec0c33d2edd956784fcbda331b2369cbdfee4da3
Aylonpinto <aylon@theselectionlab.com>
2024-10-16 22:25:55 +0200

    hardcode url

--- React/src/api/Api.ts
+++ React/src/api/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_BE_URL ?? "http://localhost:8000";
+const url = 'https://homegame-api.pintopi.nl';
 
 const api = axios.create({
   baseURL: url,
